6ed26fcd66e5f6b9205e90da75b39bda5dd1d3c6
RyoichiTakeuchi <soft.natural.1228@gmail.com>
2018-04-16 20:01:35 +0900

    Initial commit

---
+++ Assets/MarretController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarretController : MonoBehaviour {
+    public Vector3 acceleration;//加速度
+    public float m_speed;
+
+    Vector3 currentPos;
+	// Use this for initialization
+	void Start () {
+        currentPos = this.gameObject.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+            m_speed=((this.gameObject.transform.position-currentPos)/Time.deltaTime).magnitude;
+            acceleration = (this.gameObject.transform.position - currentPos) ;
+            currentPos = this.gameObject.transform.position;
+    }
+    /// <summary>
+    /// ここからドラッグ
+    /// </summary>
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        //カメラから見たオブジェクトの現在位置を画面位置座標に変換
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        //取得したscreenPointの値を変数に格納
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+
+        //オブジェクトの座標からマウス位置(つまりクリックした位置)を引いている。
+        //これでオブジェクトの位置とマウスクリックの位置の差が取得できる。
+        //ドラッグで移動したときのずれを補正するための計算だと考えれば分かりやすい
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(x, y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        //ドラッグ時のマウス位置を変数に格納
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+
+       // Debug.Log(x.ToString() + " - " + y.ToString());
+
+        //ドラッグ時のマウス位置をシーン上の3D空間の座標に変換する
+        Vector3 currentScreenPoint = new Vector3(x, y, screenPoint.z);
+
+        //上記にクリックした場所の差を足すことによって、オブジェクトを移動する座標位置を求める
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + offset;
+
+        //オブジェクトの位置を変更する
+        transform.position = currentPosition;
+    }
+}
---
+++ Assets/PackControll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackControll : MonoBehaviour {
+    float speed =30;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Marret")
+        {
+            rb.AddForce(collision.gameObject.GetComponent<MarretController>().acceleration *collision.gameObject.GetComponent<MarretController>().m_speed*speed);
+        }
+    }
+}
